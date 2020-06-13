8a71a09fb9ad936d075eac7ef78ec8d07de8c42f
Kuanch <sixigma6@gmail.com>
2020-06-13 19:32:38 +0800

    pi_sentry/feature: GeneratorExit to check if browser is on

--- pi-sentry/controller/controller.py
+++ pi-sentry/controller/controller.py
@@ -4,20 +4,26 @@ from queue import Empty
 import multiprocessing as mp
 
 
-def agent_api(agent_name, action, *arg, **karg):
-    queue = mp.Queue()
-    finished = mp.Event()
+class Agent(object):
+    def __init__(self, agent_name, action, *arg, **karg):
+        self.queue = mp.Queue()
+        self.finished = mp.Event()
 
-    agent_path = 'src.' + agent_name
-    agent = importlib.import_module(agent_path)
-    agent_action = getattr(agent, action)(queue, finished, minutes=1)
-    agent_action.start()
+        agent_path = 'src.' + agent_name
+        agent = importlib.import_module(agent_path)
+        self.agent_action = getattr(agent, action)(self.queue, self.finished, minutes=1)
+        self.agent_action.start()
 
-    while not finished.is_set():
+    def stream(self):
         try:
-            frame = queue.get(False)
-        except Empty:
-            continue
-        else:
-            yield(b'--frame\r\n'
-                  b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            while not self.finished.is_set():
+                try:
+                    frame = self.queue.get(False)
+                except Empty:
+                    continue
+                yield(b'--frame\r\n'
+                      b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        except GeneratorExit:
+            self.agent_action.terminate()
+            if not self.agent_action.is_alive():
+                self.agent_action.join()
